39b83deccdb374dd4442442175fb454782d5ea50
Eidraffin Islam <eidraffin@gmail.com>
2024-10-06 19:51:31 +0600

    update sendEmail string

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "process.env.EMAIL",
+      to: process.env.EMAIL || "default@example.com",
       subject: "Message from contact form",
       replyTo: senderEmail,
       react: React.createElement(ContactFormEmail, {

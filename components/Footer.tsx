b48b722a52b4f8a24fb3948a2bb494c9881d678d
Eidraffin Islam <eidraffin@gmail.com>
2024-10-06 20:01:33 +0600

    link added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 font-medium text-center text-gray-500">
       <div className="mb-2 block text-xs">
-        &copy; {new Date().getFullYear()} <a href="">eidraffin</a>. All rights reserved.
+        &copy; {new Date().getFullYear()} <a href="https://eidraffin.github.io/">eidraffin</a>. All rights reserved.
       </div>
       <p className="text-xs">
       </p>

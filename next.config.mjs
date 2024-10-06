04d3c25eae6f115e3cb6b3bcfd3935441c29409a
Eidraffin Islam <eidraffin@gmail.com>
2024-10-06 19:32:46 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'next';
-
-export default defineConfig({
-    reactStrictMode: true,
-    // Additional configuration options here
-});
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig

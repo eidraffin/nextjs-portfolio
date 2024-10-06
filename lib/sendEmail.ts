c67d27c1933558755207a1342a6fb7076e6493f7
Eidraffin Islam <100798293+eidraffin@users.noreply.github.com>
2024-10-06 19:00:09 +0600

    Update sendEmail.ts

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "eidrafinislam912@gmail.com",
+      to: "process.env.EMAIL",
       subject: "Message from contact form",
       replyTo: senderEmail,
       react: React.createElement(ContactFormEmail, {
